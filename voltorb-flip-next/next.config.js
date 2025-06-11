d2fecb46c0b9cd13cbd2919cb6320862b6689af8
Abhinav Krishniah <abhinavk@email.sc.edu>
2025-06-11 18:15:20 -0400

    fixed Next.js config for Vercel deployment

--- voltorb-flip-next/next.config.js
+++ voltorb-flip-next/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     unoptimized: true, // Since we're using local images, we can disable optimization
   },
